60111badcb5edf9375b78976e3f006509238a6b4
Yulian302 <bohomolyulian3022003@gmail.com>
2026-01-11 18:08:31 +0100

    dev: add /files router, configure vite build options

--- src/App.tsx
+++ src/App.tsx
@@ -6,55 +6,51 @@ import UploadPage from "./pages/UploadPage"
 import LoginPage from "./pages/LoginPage"
 import PageNotFound from "./pages/PageNotFound"
 import RegisterPage from "./pages/RegisterPage"
-import { Provider } from "react-redux"
-import store from "./store"
 import AuthRedirect from "./components/AuthRedirect"
 import FilesPage from "./pages/FilesPage"
 
 function App() {
   return (
-    <Provider store={store}>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/register"
-              element={
-                <AuthRedirect>
-                  <RegisterPage />
-                </AuthRedirect>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <AuthRedirect>
-                  <LoginPage />
-                </AuthRedirect>
-              }
-            />
-            <Route
-              path="/upload"
-              element={
-                <ProtectedRoute>
-                  <UploadPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/files"
-              element={
-                <ProtectedRoute>
-                  <FilesPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/" element={<Navigate to="/login" />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-    </Provider>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/register"
+            element={
+              <AuthRedirect>
+                <RegisterPage />
+              </AuthRedirect>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <AuthRedirect>
+                <LoginPage />
+              </AuthRedirect>
+            }
+          />
+          <Route
+            path="/upload"
+            element={
+              <ProtectedRoute>
+                <UploadPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/files"
+            element={
+              <ProtectedRoute>
+                <FilesPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
